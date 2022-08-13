358cc65f4511fc86f9993aa234ccfe6e9e143116
youbaidjis <youba.etud@gmail.com>
2022-08-13 10:16:57 -0700

    commit tree bbeae7fd4c357ba64b0b54a87e7d6c14b6eee0e5

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  get '/messages' do 
+      messages = Message.all 
+      messages.to_json
+  end
   
 end
